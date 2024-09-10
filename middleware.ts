d7b408b55162c977bfd46d288e76d5c9f33841e1
Your Name <your-email@example.com>
2024-09-10 19:03:41 +0530

    commit

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+
+
+export default authMiddleware({
+  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+});
 
 export const config = {
   matcher: [

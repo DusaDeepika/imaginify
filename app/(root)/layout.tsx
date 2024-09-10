e68c8e20ec5ad2eee9bddb960dbcf70fa21582fd
Your Name <your-email@example.com>
2024-09-10 18:20:09 +0530

    commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 // import { Toaster } from '@/components/ui/toaster'
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
+import { Toaster } from '@/components/ui/toaster'
 import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
@@ -15,7 +16,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         </div>
       </div>
       
-      {/* <Toaster /> */}
+      <Toaster />
     </main>
   )
 }

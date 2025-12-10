a747370f34c4cb0da774d63b758fc545c86fac17
DusaDeepika <21je0326@iitism.ac.in>
2025-12-10 13:35:06 +0530

    Fix ObjectId to string conversion

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -57,7 +57,8 @@ export const Collection = ({
       {images.length > 0 ? (
         <ul className="collection-list">
           {images.map((image) => (
-            <Card image={image} key={image._id as string} />
+            <Card image={image} key={image._id?.toString()} />
+
           ))}
         </ul>
       ) : (
